4b0ca8d613d10c7b4ff59f3a254d93013d505013
eric h <eric@looprecur.com>
2012-08-06 16:20:04 -0500

    making the requester able to use either http or https library to make get requests

--- requester.js
+++ requester.js
@@ -1,19 +1,40 @@
-var http = require("http")
-	, url = require('url')
+var url = require('url')
 	, querystring = require("querystring");
+
+
+var urlParser = function(str){
+	var site = url.parse(str) 
+		,	protocol_lib = site.protocol.replace(/\:$/, '');
+	if(site.port){
+		site.default_port = site.port;
+	} else if(protocol_lib == "https") {
+		site.default_port = 443;
+	} else {
+		site.default_port = 80; 
+	}
+	site.connection_lib = require(protocol_lib);
+	return site;
+}
 
 
 get = function(str, headers, cb) {
 	console.log(str);
-	var site = url.parse(str)
-		, connection = http.createClient((site.port || 80), site.hostname)
-		, path = site.pathname;
+	var site = urlParser(str)
+		, path = site.pathname
+		, http = site.connection_lib;
 		
 		console.log(headers);
 		
 		if(site.query) path += "?" + site.query;
-		var request = connection.request("GET", path, {'host' : site.host})
 		
+		var get_options = {  
+		  host: site.host,  
+		  port: site.default_port,
+		  path: site.pathname,  
+		  method: 'GET'
+		};
+
+	var request = http.request(get_options);	
 
 		request.on('response', function(response){
 		    var data = '';
@@ -31,13 +52,14 @@ get = function(str, headers, cb) {
 
 post = function(str, params, headers, cb) {
   console.log(str);
-	var site = url.parse(str);
-	  var newOnes = JSON.stringify(params);
-	  params = JSON.parse(newOnes).json;
+	var site = urlParser(str)
+		, http = site.connection_lib;
+	var newOnes = JSON.stringify(params);
+			params = JSON.parse(newOnes).json;
 		
 		var post_options = {  
 		  host: site.host,  
-		  port: 80,
+		  port: site.default_port,
 		  path: site.pathname,  
 		  method: 'POST',
 		  headers: {
